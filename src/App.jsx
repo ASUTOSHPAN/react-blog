af33a612198712f51d97c40a838926731b5e0388
Asutosh <asutoshpanda268@gmail..com>
2025-10-04 15:22:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Apple from './header'
+
+function App() {
+  return (
+    <>
+    <h1>Hello, JavaScript</h1>
+    <Apple/>
+    </>
+  )
+}
+
+export default App
